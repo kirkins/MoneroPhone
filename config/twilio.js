6058c76f11e6afa838cda2ed7e1d41aa27395efe
Philip Kirkbride <kirkins@gmail.com>
2016-08-18 13:09:38 -0500

    remove expired twilio keys

--- config/twilio.js
+++ config/twilio.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'AccountSid'	: 'ACcd63ef5050105cf91e73ac03dd7cc38f',
-  'AuthToken'	: '16001902b3106c3bf75c12eeaebd2b9d',
-  'TwilioNumber' : '1-855-801-9658'
+  'AccountSid'	: 'xxx',
+  'AuthToken'	: 'xxx',
+  'TwilioNumber' : '1-855-888-8888'
 }
